f94c953ab8c03dac1e31ca1b41919517dc83e0cb
kouski <72550619+kouski@users.noreply.github.com>
2025-02-13 17:35:39 +0100

    nuevos componentes, icnos y id

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react";
+import JobInfo from "./JobInfo";
+
 const url = 'https://www.course-api.com/react-tabs-project';
 
 const App = () => {
-  return <h2>Tabs Starter</h2>;
+
+const[isLoading,setIsLoading] = useState(true);
+const[jobs,setJobs] = useState([]);
+
+
+const fetchJobs = async ()=>{
+  const response = await fetch(url);
+  const newJobs = await response.json();
+  setJobs(newJobs)
+  setIsLoading(false)
+}
+
+useEffect(()=>{
+fetchJobs()
+},[])
+
+if(isLoading){
+  return <section className="jobs-center">
+    <div className="loading"></div>
+  </section>
+}
+
+
+
+  return <section className="jobs-center">
+  <JobInfo jobs={jobs}/>
+  </section>;
 };
 export default App;
---
+++ src/JobInfo.jsx
@@ -0,0 +1,18 @@
+import Duties from "./Duties"
+
+
+const JobInfo = ({jobs}) => {
+  
+    const{company,dates,duties,title}= jobs[1]
+  
+    return (
+    <article className="job-info">
+        <h3>{title}</h3>
+        <span className="job-company">{company}</span>
+        <p className="job-date">{dates}</p>
+        <Duties duties={duties}/>
+    </article>
+  )
+}
+
+export default JobInfo
